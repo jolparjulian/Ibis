acc9af28dde3d985a640b44e186f6b3dc8fdfcd1
jules <jolparjulian@gmail.com>
2025-10-12 17:36:15 -0400

    bug fix

--- bug.py
+++ bug.py
@@ -17,22 +17,21 @@ class Bug:
     __schwifty = Shifter(serialPin, clockPin, latchPin)
 
     def start(self):
-        while True:
-            self.__schwifty.shiftByte(1 << self.x)
-            time.sleep(self.timestep)
-            if (not self.isWrapOn):
-                if (self.x == 0):
-                    self.x = 1
-                elif (self.x == 7):
-                    self.x = 6
-                else:
-                    self.x += random.choice([-1, 1])
+        self.__schwifty.shiftByte(1 << self.x)
+        if (not self.isWrapOn):
+            if (self.x == 0):
+                self.x = 1
+            elif (self.x == 7):
+                self.x = 6
             else:
                 self.x += random.choice([-1, 1])
-                if (self.x == -1):
-                    self.x = 7
-                elif (self.x == 8):
-                    self.x = 0    
+        else:
+            self.x += random.choice([-1, 1])
+            if (self.x == -1):
+                self.x = 7
+            elif (self.x == 8):
+                self.x = 0    
+        time.sleep(self.timestep)
             
     def stop(self):
         self.__schwifty.shiftByte(0)
