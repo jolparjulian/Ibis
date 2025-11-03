31b8f959a3f1b26bc3c3b0bd7c8049e7e1a8c93c
jules <jolparjulian@gmail.com>
2025-11-03 15:46:17 -0500

    delay inc

--- stepper_class_shiftregister_multiprocessing.py
+++ stepper_class_shiftregister_multiprocessing.py
@@ -75,7 +75,7 @@ class Stepper:
 
     # Move relative angle from current position:
     def rotate(self, delta):
-        time.sleep(0.1)
+        time.sleep(0.2)
         p = multiprocessing.Process(target=self.__rotate, args=(delta,))
         p.start()
 
