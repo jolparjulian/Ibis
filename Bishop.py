485d23b3f4424a028fac46c4ed9f4f14ba682566
jules <jolparjulian@gmail.com>
2025-11-05 16:44:44 -0500

    pin fix

--- Bishop.py
+++ Bishop.py
@@ -2,10 +2,22 @@ import time
 import RPi.GPIO as gpio
 from shifter import Shifter
 
-sch = Shifter(16, 20, 21)
+serialPin = 16
+latchPin = 20
+clockPin = 21
+
+gpio.setup(serialPin, gpio.OUT)
+gpio.setup(latchPin, gpio.OUT, initial = 0)
+gpio.setup(clockPin, gpio.OUT, initial = 0)
+
+sch = Shifter(serialPin, clockPin, latchPin)
 seq = [0b00010000,0b00110000,0b00100000,0b01100000,0b01000000,0b11000000,0b10000000,0b10010000]
 
-while True:
-    for s in seq:
-        sch.shiftByte(s)
-        time.sleep(0.01)
+try:
+    while True:
+        for s in seq:
+            sch.shiftByte(s)
+            time.sleep(0.01)
+        
+except:
+    gpio.cleanup()
