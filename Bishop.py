1243135807c62e55d7fd965cd118452cdd5da8fc
jules <jolparjulian@gmail.com>
2025-11-05 16:48:31 -0500

    no half step

--- Bishop.py
+++ Bishop.py
@@ -11,7 +11,7 @@ gpio.setup(latchPin, gpio.OUT, initial = 0)
 gpio.setup(clockPin, gpio.OUT, initial = 0)
 
 sch = Shifter(serialPin, clockPin, latchPin)
-seq = [0b00010000,0b00110000,0b00100000,0b01100000,0b01000000,0b11000000,0b10000000,0b10010000]
+seq = [0b00010000,0b00100000,0b01000000,0b10000000]
 
 try:
     while True:
