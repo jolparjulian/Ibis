986aebf1b93effbea31232e79bdb332f51d14fe8
jules <jolparjulian@gmail.com>
2025-11-05 16:42:28 -0500

    my turn

--- Bishop.py
+++ Bishop.py
@@ -5,3 +5,7 @@ from shifter import Shifter
 sch = Shifter(16, 20, 21)
 seq = [0b00010000,0b00110000,0b00100000,0b01100000,0b01000000,0b11000000,0b10000000,0b10010000]
 
+while True:
+    for s in seq:
+        sch.shiftByte(s)
+        time.sleep(0.01)
