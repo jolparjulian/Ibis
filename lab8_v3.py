f0e24202829a5171795500f891e822a02b9b65c5
jules <jolparjulian@gmail.com>
2025-11-11 19:53:02 -0500

    joinng

--- lab8_v3.py
+++ lab8_v3.py
@@ -81,6 +81,7 @@ class Stepper:
         time.sleep(0.1)
         p = multiprocessing.Process(target=self.__rotate, args=(delta,))
         p.start()
+        p.join()
 
     # Move to an absolute angle taking the shortest possible path:
     def goToAngle(self, angle):
