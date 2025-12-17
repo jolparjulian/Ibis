a321980ccc6d4cdbe6c05f3188ae4367ce33b774
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-12-17 01:09:10 -0500

    Add files via upload

--- Project/turret_code.py
+++ Project/turret_code.py
@@ -27,7 +27,7 @@ vert.start_process()
 hor.start_process()
 
 # Positional variables
-cyl_position = [172.4,np.radians(299.68),8.23] # r t z
+cyl_position = [172.4,np.radians(299.68),4] # r t z
 position = [0,0,0] # disregard
 ref_positions = [] # place r/t/z/stepper angles into here to math later
 angle = [0,0] #pitch/yaw
