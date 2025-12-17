1d7e6f90dd273a7375429e2b3b993288960cdefc
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-12-16 22:30:36 -0500

    Add files via upload

--- Project/turret_code.py
+++ Project/turret_code.py
@@ -12,7 +12,7 @@ from stepper import Stepper #grab stepper class
 
 #Set up GPIOs
 GPIO.setmode(GPIO.BCM)
-laser_pin = 17 #check this
+laser_pin = 26 #check this
 GPIO.setup(laser_pin, GPIO.OUT)
 laser_time = 2.5
 
@@ -32,7 +32,7 @@ position = [0,0,0] # disregard
 ref_positions = [] # place r/t/z/stepper angles into here to math later
 angle = [0,0] #pitch/yaw
 # replace the second number with our degrees position
-offset = 360-57 # so we can reference yaw angle to center
+offset = 360-301 # so we can reference yaw angle to center
 
 json_data = [] # to put json in later
 
@@ -687,4 +687,3 @@ def run_server():
 
 if __name__ == "__main__":
 	run_server()
-
